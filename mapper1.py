9a280f4c8951f6104c64c7b6c98f4dc73f72ea4c
maryamkhalid <frostbite3k@gmail.com>
2024-03-31 23:16:48 +0500

    Update mapper1.py

--- mapper1.py
+++ mapper1.py
@@ -6,22 +6,27 @@ import re
 
 # tokenizes text
 def tokenize_text(text):
+     #tokenize text by finding words
     tokens = re.findall(r'\b[A-Za-z]+\b', text.lower())
     return tokens
 
 def mapper():
     csv_reader = csv.reader(sys.stdin)
-    word_id_mapping = {}  # map word to unique ID
-    current_id = 0  # current ID counter
-    
+    word_id_mapping = {}  # dictionary to map words to unique IDs
+    current_id = 0  #  # initialize current ID counter
+
+    #iterate over each row in the CSV input
     for row in csv_reader:
-        if len(row) == 2:
+        if len(row) == 2:   #row has two elements
             section_text = row[1]
+             #tokenize the text of the article
             tokens = tokenize_text(section_text)
             for token in tokens:
                 if token not in word_id_mapping:
+                    # assign a unique ID to each word
                     word_id_mapping[token] = current_id
                     current_id += 1
+                     # output word ID and article ID with frequency
                 print(f"{word_id_mapping[token]}\t{row[0]}:1")  #emit word ID and article ID with frequency
 
 if __name__ == "__main__":
